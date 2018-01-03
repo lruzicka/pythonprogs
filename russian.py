1f6540188503a4f84c8cf0bee13640eded45a806
Lukáš Růžička <lruzicka@redhat.com>
2018-01-03 22:38:10 +0100

    Add functionality to the game.

--- russian.py
+++ russian.py
@@ -17,10 +17,13 @@ def rotate(drum):
 
 def shoot(drum):
     """Shoot and see"""
-    trigger = drum[0]
+    trigger = drum.pop(0)
+    drum.append(0)
     return(trigger)
         
+#-------------------------------------------------------------------------------------------
 
+print("Welcome to the game of courageous tsars. Take your gun and let's play!")
 
 slots = input("How many slots does your gun have?: ")
 slots = int(slots)
@@ -29,16 +32,30 @@ bullets = input("How many bullets do you want to load?: ")
 bullets = int(bullets)
 
 if bullets < slots:
-    input("Press the trigger!")
-
-    drum = load(slots,bullets)
+    risk = True
+    drum = load(slots, bullets)
     drum = rotate(drum)
-    result = shoot(drum)
-
-    if result == 1:
-        print("You die!")
-    else:
-        print("You survived!")
+    
+    while risk == True:
+        input("Press the trigger!")
+        result = shoot(drum)
+            
+        if result != 1:
+            print("You survived!")
+            decision = input("Do you want to continue? (y/N): ")
+            if decision == 'y':
+                risk = True
+                decision = input("Do you want to be a coward and rotate the drum again? (y/N): ")
+                if decision == 'y':
+                    drum = rotate(drum)
+            else:
+                    risk = False
+                    print("You coward! Take your shitty pants out of this company!")
+            
+        else:
+            print("You painted the walls with your brain!")
+            risk = False
+            
     
 else:
     print("You are stupid, you cannot load so many bullets. You only have {} slots.".format(slots))
