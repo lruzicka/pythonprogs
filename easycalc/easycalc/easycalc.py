ddd5dd9d32018e637bb1a09182fda44cc873954d
Lukas Ruzicka <lruzicka@redhat.com>
2018-01-25 17:31:38 +0100

    Try to fix.

--- easycalc/easycalc/easycalc.py
+++ easycalc/easycalc/easycalc.py
@@ -9,24 +9,23 @@ def out(text):
     for line in text:
         print(line)
 
-def main():
-    welcome = ["A very simple calculator",
+
+welcome = ["A very simple calculator",
                "----------------------------------"]
-    middle = ["-----------------------------------"]
-    bye = ["-----------------------------------", 
-           "Good bye!"]
+middle = ["-----------------------------------"]
+bye = ["-----------------------------------", 
+       "Good bye!"]
 
-    if len(sys.argv) > 1:
-        print(eval(sys.argv[1]))
-    else:
-        loop = 'y'
-        out(welcome)
-        while loop == 'y':
-            result = eval(input("Write your expression: "))
-            print("Result: ",result)
-            out(middle)
-            loop = input("New expression? (y/n): ")
-            loop = loop.lower()
-        out(bye)
+if len(sys.argv) > 1:
+    print(eval(sys.argv[1]))
+else:
+    loop = 'y'
+    out(welcome)
+    while loop == 'y':
+        result = eval(input("Write your expression: "))
+        print("Result: ",result)
+        out(middle)
+        loop = input("New expression? (y/n): ")
+        loop = loop.lower()
+    out(bye)
 
-app = main()
